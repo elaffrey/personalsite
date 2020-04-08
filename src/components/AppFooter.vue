e1ab565122c78bb23a6d6fad9e536124acfeb2f8
Emily <elaff1117@gmail.com>
2020-04-08 08:02:20 -0700

    init project and removed unnecessary files

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card flat>
+    <v-footer 
+      bottom
+      app
+      padless
+    >
+      <v-card
+        class="flex"
+        color="teal"
+      >
+        <v-card-title>
+          {{ 'Emilys\' Page' }}
+          <v-spacer />
+        </v-card-title>
+      </v-card>
+    </v-footer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+};
+</script>
