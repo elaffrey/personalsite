9467178039ff2bbe68ba5cb572f1977e42c3441c
Emily <elaff1117@gmail.com>
2020-04-14 09:40:56 -0700

    changed icons

---
+++ src/components/ParentTab.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-tabs
+      class="mb-2"
+      centered
+      v-model="activeTab"
+      background-color="transparent"
+      slider-color="primary"
+    >
+      <v-tab
+        v-for="tab in tabs"
+        :key="tab.id"
+        :to="tab.route"
+        exact
+      >
+        {{ tab.title }}
+      </v-tab>
+    </v-tabs>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ParentTab',
+  props: {
+    tabs: { type: Array, required: true }
+  },
+  data: () => ({
+    activeTab: ''
+  })
+}
+</script>
